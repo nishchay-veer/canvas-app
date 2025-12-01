16dd816a3db1ac39a0132f1334135555f8f69865
Nishchay Veer <nishchayveer19@gmail.com>
2025-12-01 14:46:20 +0530

    added db package
    Signed-off-by: Nishchay Veer <nishchayveer19@gmail.com>

---
+++ packages/db/src/index.ts
@@ -0,0 +1,18 @@
+import { PrismaClient } from "@prisma/client";
+
+const prismaClientSingleton = () => {
+  return new PrismaClient();
+};
+
+declare global {
+  var prismaGlobal: undefined | ReturnType<typeof prismaClientSingleton>;
+}
+
+export const prisma = globalThis.prismaGlobal ?? prismaClientSingleton();
+
+if (process.env.NODE_ENV !== "production") {
+  globalThis.prismaGlobal = prisma;
+}
+
+// Re-export Prisma types
+export * from "@prisma/client";
