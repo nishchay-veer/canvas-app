2f5363b421969958687cde8e945268a0b77a732d
Nishchay Veer <nishchayveer19@gmail.com>
2026-01-02 19:15:08 +0530

    setting up a CI/CD pipeline
    Signed-off-by: Nishchay Veer <nishchayveer19@gmail.com>

--- apps/web/app/config.ts
+++
@@ -1,4 +0,0 @@
-export const API_BASE_URL = process.env.API_BASE_URL || "http://localhost:3001";
-export const WS_BASE_URL =
-  process.env.WS_BASE_URL ||
-  "ws://localhost:8080?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjMzYTkzNWE1LTVhMTEtNGM5OC1iMDJlLTgyODdhM2QyZjg4MiIsInVzZXJuYW1lIjoidGhlYWxvbmVtdXNrIiwiaWF0IjoxNzY1MDUwMDY2LCJleHAiOjE3NjUxMzY0NjZ9.XZraVWC1Ew3KFGhuSR-mjEfR7klDTewF9-cySSUjaWQ";
